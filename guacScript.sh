01fb63012aa283894da68595cd7a6a977c7450c0
soupsamich <66322946+soupsamich@users.noreply.github.com>
2020-06-23 18:17:41 -0400

    Round of fixes and syntax changes
    I believe these fixes should work if not very close to fixing everything.
    These changes have only been tested directly on the server, not yet tested while nested inside of the deployment script.

--- guacScript.sh
+++ guacScript.sh
@@ -3,6 +3,7 @@
 apt-get update
 # {create the nonroot user account in a way which can be scripted (pretty sure useradd instead of adduser, but then we need to set their default shell and passwd manually)}
 password="This is a basic student password"
+username="student"
 pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
 useradd -m -p "$pass" "$username"
 # installing guacmole
@@ -11,17 +12,19 @@ apt-get install guacamole-tomcat
 cd /var/lib/tomcat8/webapps/
 wget http://apache.org/dyn/closer.cgi?action=download&filename=guacamole/1.1.0/binary/guacamole-1.1.0.war
 touch /etc/tomcat8/Catalina/localhost/guacamole.xml
-echo "<Context path="/guacamole" docbase="/var/lib/tomcat8/webapps/guacamole.war"" > /etc/tomcat8/Catalina/localhost/guacamole.xml
+echo '<Context path="/guacamole" docbase="/var/lib/tomcat8/webapps/guacamole.war">' > /etc/tomcat8/Catalina/localhost/guacamole.xml
 echo " <Resources allowLinking='true' />" >> /etc/tomcat8/Catalina/localhost/guacamole.xml
-echo "</Context>" >> /etc/tomcat8/localhost/guacamole.xml
-apt-get install xvnc4server
+echo "</Context>" >> /etc/tomcat8/Catalina/localhost/guacamole.xml
+apt-get install vnc4server
+mkdir /etc/vncserver
 touch /etc/vncserver/vncservers.conf
 echo "VNCSERVERS="1:student"" > /etc/vncserver/vncservers.conf
 echo "VNCSERVERARGS[1]="-geometry 1600x900"" >> /etc/vncserver/vncservers.conf
 mypasswd="here is the vnc password for this student account"
 mkdir /home/student/.vnc
-echo $mypasswd | vncpasswd -f > /home/$myuser/.vnc/passwd
-chown -R student:student /home/$myuser/.vnc
+# echo $mypasswd | vncpasswd -f > /home/student/.vnc/passwd
+printf "$mypasswd\n$mypasswd\n\n" | vnc4passwd /home/student/.vnc/passwd
+chown -R student:student /home/student/.vnc
 chmod 0600 /home/student/.vnc/passwd
 touch /home/student/.vnc/xstartup
 echo '#!/bin/sh' >> /home/student/.vnc/xstartup
