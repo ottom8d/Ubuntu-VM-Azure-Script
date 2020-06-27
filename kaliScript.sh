c6c7e995cdc28a4940b12a0f85f0aeae4ea18a08
soupsamich <66322946+soupsamich@users.noreply.github.com>
2020-06-27 16:16:04 -0400

    Update kaliScript.sh

--- kaliScript.sh
+++ kaliScript.sh
@@ -11,23 +11,23 @@ cd /labs/john-jumbo/john/src
 ./configure && make -s clean && make -sj4
 wget http://64.227.19.44/content/dlj23jjoj233jjajdeqo/hashes -O /labs/hashID/hashes
 wget http://64.227.19.44/content/dlj23jjoj233jjajdeqo/winMetListener.sh -O /labs/gatherHashes/winMetListener.sh
-chmod +x winMetListener.sh
+chmod +x /labs/gatherHashes/winMetListener.sh
 wget http://64.227.19.44/content/dlj23jjoj233jjajdeqo/winNCListener.sh -O /labs/gatherHashes/winNCListener.sh
-chmod +x winNCListener.sh
+chmod +x /labs/gatherHashes/winNCListener.sh
 wget http://64.227.19.44/content/dlj23jjoj233jjajdeqo/linuxMetListener.sh -O /labs/gatherHashes/linuxMetListener.sh
-chmod +x linuxMetListener.sh
+chmod +x /labs/gatherHashes/linuxMetListener.sh
 wget http://64.227.19.44/content/dlj23jjoj233jjajdeqo/handler.rc -O /root/handler.rc
 wget http://64.227.19.44/content/dlj23jjoj233jjajdeqo/launcher.sh -O /root/launcher.sh
 printf "dh?w2qa3\ndh?w2qa3\n\n" | vncpasswd -f
 echo '[Unit]' > /etc/systemd/system/tightvncserver.service
 echo 'Description=TightVNC remote desktop server' >> /etc/systemd/system/tightvncserver.service
-echo -e '\n' >> /etc/systemd/system/tightvncserver.service
+echo \n' >> /etc/systemd/system/tightvncserver.service
 echo '[Service]' >> /etc/systemd/system/tightvncserver.service
 echo 'Type=dbus' >>  /etc/systemd/system/tightvncserver.service
 echo 'ExecStart=/usr/bin/tightvncserver :1' >>  /etc/systemd/system/tightvncserver.service
 echo 'User=HashCrackUser1' >> /etc/systemd/system/tightvncserver.service
 echo 'Type=forking' >> /etc/systemd/system/tightvncserver.service
-echo -e '\n' >> /etc/systemd/system/tightvncserver.service
+echo '\n' >> /etc/systemd/system/tightvncserver.service
 echo '[Install]' >>  /etc/systemd/system/tightvncserver.service
 echo 'WantedBy=multi-user.target' >>  /etc/systemd/system/tightvncserver.service
 chown root:root /etc/systemd/system/tightvncserver.service
